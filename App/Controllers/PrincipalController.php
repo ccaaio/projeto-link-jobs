922c8b3a1e295025863f065104455e024b5f7eb6
Albson-PC\Albson <andre.tecladist@gmail.com>
2019-10-16 19:10:14 -0300

    UP

--- App/Controllers/PrincipalController.php
+++ App/Controllers/PrincipalController.php
@@ -14,20 +14,15 @@ class PrincipalController extends Controller {
     public function index()
     {
 
-        $this->existeLayout(false);
+        self::setViewParam('nameController',$this->app->getNameController());
 
-
-        elf::setViewCss('/public/css/flatpickr.min.css');
+        self::setViewCss('/public/css/flatpickr.min.css');
         self::setViewCss('/public/css/slick.css');
         self::setViewCss('/public/css/slick-theme.css');
 
         self::setViewJs('/public/js/slick.min.js');
 
-        $this->redirect('principal/');
-
-        self::setViewParam('nameController', $this->app->getNameController());
-
-        $this->render('login/index');
+        $this->render('principal/index');
 
     }
 
