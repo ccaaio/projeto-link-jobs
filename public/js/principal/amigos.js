7300aa6a41cc726af93754767277823ed6617472
Albson-PC\Albson <andre.tecladist@gmail.com>
2019-10-23 17:10:32 -0300

    implementação -> Página de Amigos/Solicitações de Amizade [BACK-END]

--- public/js/principal/amigos.js
+++ public/js/principal/amigos.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     var userLogado = $('#id-logado').val();
+    var userLogado2 = $('#id-logado').val();
 
     //obtem a lista de usuarios do bd e exibe em sugestoes
     $.ajax({
@@ -12,22 +13,24 @@ $(document).ready(function () {
     });
 
     //ao clicar em aceitar solicitação de amizade...
-    $('.aceitar-solicitacao').on('click', function () {
-       var idAceitado = $(this).attr('data-id-usuario-solicitante');
-       var nomeAceitado = $(this).attr('data-nome-usuario-solicitante');
-       var nomeUsuarioLogado = $('#nome-user-logado-titulo').text();
+    setTimeout(function () {
+        $('.aceitar-solicitacao').on('click', function () {
+            var idAceitado = $(this).attr('data-id-usuario-solicitante');
+            var nomeAceitado = $(this).attr('data-nome-usuario-solicitante');
+            var nomeUsuarioLogado = $('#nome-user-logado-titulo').text();
 
-        var item = $(this).parent().parent().parent().parent().parent().parent().addClass('animated fadeOut');
+            var item = $(this).parent().parent().parent().parent().parent().addClass('animated fadeOut');
 
-        $.ajax({
-            url:"/principal/processaAceitacaoAmizade/",
-            method:"POST",
-            data:{idAceitado: idAceitado, nomeAceitado: nomeAceitado, idLogado: userLogado, nomeLogado: nomeUsuarioLogado},
-            success:function(p){
-                setTimeout(function () {
-                    $(item).remove();
-                },300);
-            }
+            $.ajax({
+                url:"/principal/processaAceitacaoAmizade/",
+                method:"POST",
+                data:{idAceitado: idAceitado, nomeAceitado: nomeAceitado, idLogado: userLogado2, nomeLogado: nomeUsuarioLogado},
+                success:function(p){
+                    setTimeout(function () {
+                        $(item).remove();
+                    },300);
+                }
+            });
         });
-    });
+    },1000);
 });
