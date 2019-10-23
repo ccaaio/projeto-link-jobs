eee3b4a3f402a1efe23ebd57d0a8ba126515c0cf
Albson-PC\Albson <andre.tecladist@gmail.com>
2019-10-23 17:35:35 -0300

    implementação -> Página de Amigos/Solicitações de Amizade [BACK-END]

--- App/Controllers/PrincipalController.php
+++ App/Controllers/PrincipalController.php
@@ -162,7 +162,7 @@ class PrincipalController extends Controller {
             $nomeLogado = $_POST['nomeLogado'];
 
             $result = mysqli_query($conn, "INSERT INTO lista_amigos (id_solicitante, id_requisitado, nome_requisitado, nome_solicitante) VALUES ('$idLogado', '$idAceitado', '$nomeAceitado', '$nomeLogado')");
-            $result2 = mysqli_query($conn, "UPDATE amizade SET status = '1' WHERE id_solicitante = '$idLogado' AND id_requisitado = '$idAceitado'");
+            $result2 = mysqli_query($conn, "UPDATE amizade SET status = '1' WHERE id_solicitante = '$idAceitado' AND id_requisitado = '$idLogado'");
         }
     }
 
