0c5fb60a3d662360116ecfb8d914dfc012df0aba
Albson-PC\Albson <andre.tecladist@gmail.com>
2019-10-23 19:20:54 -0300

    implementação -> Página de Amigos/Solicitações de Amizade [FRONT-END]

--- App/Controllers/PrincipalController.php
+++ App/Controllers/PrincipalController.php
@@ -185,6 +185,7 @@ class PrincipalController extends Controller {
             $idLogado = $_POST['idLogado'];
 
             $result2 = mysqli_query($conn, "UPDATE amizade SET status = 1 WHERE id_solicitante = '$idLogado' AND id_requisitado = '$idAceitado'");
+            $result2 = mysqli_query($conn, "UPDATE amizade SET status = '1' WHERE id_solicitante = '$idAceitado' AND id_requisitado = '$idLogado'");
         }
     }
 
