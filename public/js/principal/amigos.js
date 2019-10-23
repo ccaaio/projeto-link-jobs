88a515fccf090125fcd4a84ee142f8bb8407a71b
Albson-PC\Albson <andre.tecladist@gmail.com>
2019-10-23 19:01:27 -0300

    implementação -> Página de Amigos/Solicitações de Amizade [FRONT-END]

--- public/js/principal/amigos.js
+++ public/js/principal/amigos.js
@@ -67,18 +67,19 @@ $(document).ready(function () {
     //ao clicar em recusar a  solicitação de amizade...
     setTimeout(function () {
         $('.recusar-solicitacao').on('click', function () {
-        var idAceitadoR = $(this).attr('data-id-usuario-solicitante');
+            var idAceitadoR = $(this).attr('data-id-usuario-solicitante');
 
-        var item2 = $(this).parent().parent().parent().parent().parent().addClass('animated fadeOut');
+            var item2 = $(this).parent().parent().parent().parent().parent().addClass('animated fadeOut');
 
-        $.ajax({
-            url:"/principal/atualizaStatusAceitacaoAmizade/",
-            method:"POST",
-            data:{idAceitado: idAceitadoR, idLogado: userLogado3},
-            success:function(rr){
-                console.log('recusado');
-                $(item2).remove();
-            }
+            $.ajax({
+                url:"/principal/atualizaStatusAceitacaoAmizade/",
+                method:"POST",
+                data:{idAceitado: idAceitadoR, idLogado: userLogado3},
+                success:function(rr){
+                    console.log('recusado');
+                    $(item2).remove();
+                }
+            });
         });
     },1000);
 });
