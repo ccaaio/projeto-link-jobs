801b8111c9564a27af6a2bb074e3915cbd656897
Albson-PC\Albson <andre.tecladist@gmail.com>
2019-10-30 11:31:37 -0300

    Upload capa perfil usuario

--- public/js/perfil/editar.js
+++ public/js/perfil/editar.js
@@ -1,16 +1,20 @@
 $(document).ready(function () {
-    var idUsuario = $('#id_user').val();
+    setTimeout(function () {
+        var idUsuario = $('#id_user').val();
 
-    //obtem a lista de usuarios do bd e exibe em sugestoes
-    $.ajax({
-        url:"/perfil/getCapaPerfil/",
-        method:"POST",
-        data:{idUser: idUsuario},
-        success:function(c){
-            $('.loader-capa').addClass('d-none');
-            var capa =c.src;
-            var trimCapa = $.trim(capa);
-            $('#img-capa').attr("src", "/public/uploads/capa/"+trimCapa+"");
-        }
-    });
+        setTimeout(function () {
+            //obtem a lista de usuarios do bd e exibe em sugestoes
+            $.ajax({
+                url:"/perfil/getCapaPerfil/",
+                method:"POST",
+                data:{idUser: idUsuario},
+                success:function(c){
+                    $('.loader-capa').addClass('d-none');
+                    var capa =c.src;
+                    var trimCapa = $.trim(capa);
+                    $('#img-capa').attr("src", "/public/uploads/capa/"+trimCapa+"");
+                }
+            });
+        },200);
+    },1000);
 });
