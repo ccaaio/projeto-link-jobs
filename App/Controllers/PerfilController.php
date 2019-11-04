63edc932868f59a032761fa6728706b71f8e5872
Albson-PC\Albson <andre.tecladist@gmail.com>
2019-11-04 13:10:17 -0300

    Atualizar Informações do User

--- App/Controllers/PerfilController.php
+++ App/Controllers/PerfilController.php
@@ -168,12 +168,8 @@ class PerfilController extends Controller {
         $visao = 'aaaa';
         $idUser = 13;
 
-        echo 'OKOKOKOK';
-
-        /*
         $conn = mysqli_connect("remotemysql.com", "GQ4OpczpAV", "jt4ifMIloM", "GQ4OpczpAV");
         mysqli_query($conn, "INSERT INTO visaoGeral (id_usuario, visao) VALUES ('$idUser', '$visao'");
-        */
     }
 
     public function getVisao() {
