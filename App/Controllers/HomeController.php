6da0ba79c9750feb1737da5a83767591dae3013d
Albson-PC\Albson <andre.tecladist@gmail.com>
2019-11-04 15:58:49 -0300

    Atualizar Informações do User

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -19,8 +19,11 @@ class HomeController extends Controller {
 
         self::setViewCss('/public/css/slick.css');
         self::setViewCss('/public/css/slick-theme.css');
+        self::setViewCss('/public/css/pages/login/login.css');
 
         self::setViewJs('/public/js/slick.min.js');
+        self::setViewJs('/public/js/jquery.mask.min.js');
+        self::setViewJs('/public/js/login/login.js');
 
         self::setViewParam('nameController', $this->app->getNameController());
 
