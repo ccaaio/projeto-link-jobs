fcd3fc5d178e37a4f8677fd2b60852d0f673b4bf
Albson-PC\Albson <andre.tecladist@gmail.com>
2019-11-06 15:25:20 -0300

    Postagem e Listagem de Vagas

--- public/js/principal/principal.js
+++ public/js/principal/principal.js
@@ -3,16 +3,16 @@ $(document).ready(function () {
 
     //obtem a lista de usuarios do bd e exibe em sugestoes
     $.ajax({
-        url:"/perfil/getCapaPerfil/",
+        url:"/perfil/getFotoPerfil/",
         method:"POST",
         data:{idUser: idLogadoFoto},
         success:function(s){
             //alert(s.foto);
             var foto =s.foto;
             var trimFoto = $.trim(foto);
-            $('.usr-pic').html('<img class="foto-perfil-user-logado" src="/public/images/perfil/'+trimFoto+'" alt="Foto do Perfil">');
-            $('.user-picy').html('<img class="foto-perfil-user-logado" src="/public/images/perfil/'+trimFoto+'" alt="Foto do Perfil">');
-            $('.foto-perfil-navbar').attr('src', '/public/images/perfil/'+trimFoto+'');
+            $('.usr-pic').html('<img class="foto-perfil-user-logado" src="/public/uploads/fotoPerfil/'+trimFoto+'" alt="Foto do Perfil">');
+            $('.user-picy').html('<img class="foto-perfil-user-logado" src="/public/uploads/fotoPerfil/'+trimFoto+'" alt="Foto do Perfil">');
+            $('.foto-perfil-navbar').attr('src', '/public/uploads/fotoPerfil/'+trimFoto+'');
         }
     });
 
