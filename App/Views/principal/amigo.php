802536c47597cd83e20a3ac7b443679cc53fb5cd
Albson-PC\Albson <andre.tecladist@gmail.com>
2019-11-09 12:11:02 -0300

    Função de Recomendar

--- App/Views/principal/amigo.php
+++ App/Views/principal/amigo.php
@@ -185,7 +185,7 @@
                                             </li>
                                             <li data-tab="feed-dd">
                                                 <a class="recomendarPerfil" data-id-perfil="<?php echo $aViewVar['aAmigo']['id']; ?>" title="" style="text-decoration: none !important;color: #b2b2b2;cursor: pointer;">
-                                                    <span id="qtd-likes" class="badge badge-pill badge-danger" style="position: absolute;color: #FFFFFF;font-size: 14px;line-height: 0.98;padding-top: 0.4em;padding-bottom: 0.4em;margin-left: 45px;margin-top: -9px;">0</span>
+                                                    <span id="qtd-likes" class="badge badge-pill badge-danger" style="position: absolute;color: #FFFFFF;font-size: 14px;line-height: 0.98;padding-top: 0.4em;padding-bottom: 0.4em;margin-left: 74px;margin-top: -9px;">0</span>
                                                     <i class="la la-thumbs-o-up" style="font-size: 34px;"></i>
                                                     <span>Recomendações</span>
                                                 </a>
