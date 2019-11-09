07255e635a11466130240fa9c1ca1d9131082f73
Albson-PC\Albson <andre.tecladist@gmail.com>
2019-11-09 14:23:55 -0300

    Função de Recomendar

--- App/Views/principal/index.php
+++ App/Views/principal/index.php
@@ -23,7 +23,7 @@
 <div class="modal fade" id="modal-confirmacao-exluir-vaga" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div class="modal-header">
+            <div class="modal-header bg-danger text-light">
                 <h5 class="modal-title" id="exampleModalLabel">Confirmação de Exclusão de Postagem</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
